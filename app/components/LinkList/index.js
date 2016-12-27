e01874b337a243ceed60d8df6ff1faf6d02cd18f
NGUYEN Nhat Minh <nath-minh.nguyen_externe@group-sma.fr>
2016-12-27 11:33:01 +0100

    Adding routes to your app

--- app/components/LinkList/index.js
+++ app/components/LinkList/index.js
@@ -10,20 +10,22 @@ import React from 'react';
 import styles from './styles.css';
 import Link from '../Link';
 
-function LinkList({links}) {
+function LinkList({links, topicName}) {
   const linkNodes = links.map(l => (
     <Link
       key={l.id}
-      link={l}/>
+      link={l} />
   ));
   return (
     <div className={styles.linkList}>
+      {topicName}
       {linkNodes}
     </div>
   );
 }
 
 LinkList.propTypes = {
+  topicName: React.PropTypes.string.isRequired,
   links: React.PropTypes.arrayOf(React.PropTypes.shape({
     description: React.PropTypes.string.isRequired,
     url: React.PropTypes.string.isRequired,
--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,8 +1,10 @@
 // import { take, call, put, select } from 'redux-saga/effects';
-import { REQUEST_TOPICS } from './constants';
+import { REQUEST_TOPICS, SELECT_TOPIC, REQUEST_TOPICS_SUCCEEDED } from './constants';
 import { takeLatest } from 'redux-saga';
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import { requestTopicsSucceeded, requestTopicsFailed } from './actions';
+import { push } from 'react-router-redux';
+import selectNavigationContainer from './selectors';
 
 export function fetchTopicsFromServer() {
   return fetch('http://localhost:3000/api/topics')
@@ -23,8 +25,28 @@ export function* fetchTopicsSaga() {
   yield* takeLatest(REQUEST_TOPICS, fetchTopics);
 }
 
+function* pushTopic(action) {
+  yield put(push(`/topics/${action.topic.name}`));
+}
+
+function* selectDefaultTopic(){
+  const state = yield select(selectNavigationContainer());
+  if(!state.selectedTopic){
+    yield put(push(`/topics/${state.topics[0].name}`));
+  }
+}
+
+export function* selectDefaultTopicSaga(){
+  yield* takeLatest(REQUEST_TOPICS_SUCCEEDED, selectDefaultTopic);
+}
+
+export function* selectTopicSaga() {
+  yield* takeLatest(SELECT_TOPIC, pushTopic);
+}
 
 // All sagas to be loaded
 export default [
+  selectTopicSaga,
   fetchTopicsSaga,
+  selectDefaultTopicSaga
 ];
